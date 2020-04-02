38f2fc77b5349cb4dd0a974e19d373fd2b6c7c26
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-02 07:46:18 -0300

    :sparkles: criando as rotas

---
+++ src/routes/Route.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  return <Route {...rest} render={props => <Component {...props} />} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
