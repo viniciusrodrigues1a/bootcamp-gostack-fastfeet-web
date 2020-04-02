e00b4892762302399862e7070e5d664ce81b37b4
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-02 13:19:57 -0300

    :sparkles: criando rotas autenticadas e aplicando layout

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+
+import DefaultLayout from '~/pages/_layouts/default';
+
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  return <Route {...rest} render={props => <Component {...props} />} />;
+  const { signed } = store.getState().auth;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/deliveries" />;
+  }
+
+  const Layout = signed ? DefaultLayout : ({ children }) => <>{children}</>;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
