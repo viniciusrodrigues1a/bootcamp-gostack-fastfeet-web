7cea139d2b3ac6d43a852244908eb8ed210750c5
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-03 15:31:16 -0300

    :sparkles: criando rota para criação de delivery

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ import Route from './Route';
 import SignIn from '~/pages/SignIn';
 
 import Deliveries from '~/pages/Deliveries';
+import NewDelivery from '~/pages/NewDelivery';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/deliveries" component={Deliveries} isPrivate />
+      <Route exact path="/deliveries/new" component={NewDelivery} isPrivate />
       <Route path="/" component={() => <Redirect to="/" />} />
     </Switch>
   );
