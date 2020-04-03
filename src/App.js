c43498d13e66423b46795e1b637abc954db4ef11
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-03 15:24:08 -0300

    :wrench: configurando react-toastify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { toast } from 'react-toastify';
 
 import '~/config/ReactotronConfig';
 
@@ -10,6 +11,10 @@ import { store, persistor } from '~/store';
 
 import GlobalStyles from '~/styles/global';
 import Routes from '~/routes';
+
+toast.configure({
+  autoClose: 3000,
+});
 
 function App() {
   return (
