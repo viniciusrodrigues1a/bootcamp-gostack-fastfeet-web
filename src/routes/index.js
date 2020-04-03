f76b2e19cdba15b00b71bdd51c45bbadab4f7596
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-04-03 08:31:28 -0300

    :sparkles: adicionando rota à listagem de deliveries

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Switch, Redirect } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
+import SignIn from '~/pages/SignIn';
+
+import Deliveries from '~/pages/Deliveries';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
+      <Route exact path="/deliveries" component={Deliveries} isPrivate />
       <Route path="/" component={() => <Redirect to="/" />} />
     </Switch>
   );
